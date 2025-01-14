71e0a698a4758cc8129ae94dd36c8c8047e7f420
hntrl <hunter@hntrl.io>
2025-01-14 16:26:57 -0700

    chore: remove extraneous cmark_gfm_extensions import

--- Sources/MarkdownUI/Parser/MarkdownParser.swift
+++ Sources/MarkdownUI/Parser/MarkdownParser.swift
@@ -1,6 +1,5 @@
 import Foundation
 @_implementationOnly import cmark_gfm
-@_implementationOnly import cmark_gfm_extensions
 
 extension Array where Element == BlockNode {
   init(markdown: String) {
